799a4514a658b25fb189cb82500e11d8299a58fb
Rafael Villanueva <rafael.villanueva0414@gmail.com>
2024-05-17 02:50:36 +0800

    user_login expirement

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 from streamlit_gsheets import GSheetsConnection
-from datetime import date
+# from datetime import date
+import hashlib
 
 st.set_page_config("KP Cases Detailed Entry")
 
@@ -14,96 +16,90 @@ css ='''
 '''
 st.markdown(css, unsafe_allow_html=True)
 
+# Hash a password
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
 # Start the connection to Google Sheets
-conn = st.connection("gsheets", type=GSheetsConnection)
+# conn = st.connection("gsheets", type=GSheetsConnection)
 users_db = st.connection("usersDB", type=GSheetsConnection)
-
-
-# ===========  Start FUNCTIONS  ====================
-
-def read_users_db(worksheet, usecols):
-    user_data = users_db.read(worksheet=worksheet, usecols=usecols)
-    user_data = user_data.dropna(how="all")
-    return user_data
-
-
-@st.cache_data(ttl=5)
+@st.cache_data(ttl=60)
 def read_data(worksheet, usecols):
-    data = conn.read(worksheet=worksheet, usecols=usecols)
+    data = users_db.read(worksheet=worksheet, usecols=usecols)
     data = data.dropna(how="all")
     return data
 
-# Fomat the dates
-def dateseq():
-    # Get the current date
-    current_date = date.today()
+users = read_data("users_db", list(range(5)))
 
-    # Format the date to 'YYYYMM'
-    formatted_date = current_date.strftime('%Y%m')
+# # Initialize an empty dictionary to store the data
+# user_dict = {}
 
-    return formatted_date
+# # user_dict = users.set_index('user')[['passwd', 'mps_cps']].T.to_dict()
+# # Iterate over the DataFrame rows as (index, Series) pairs
+# for index, row in users.iterrows():
+#     # Use the 'user' field as the dictionary key and the other fields as values in another dictionary
+#     # Hash the password before storing it in the dictionary
+#     user_dict[row['user']] = {'password': hash_password(row['passwd']), 'mps': row['mps_cps']}
 
+# Apply the hash_password function to the 'passwd' column
+users['hashed_passwords'] = users['passwd'].apply(hash_password)
 
-def login():
-    with st.form(key="login",clear_on_submit=True):
-        st.title("KP Cases Encoding Users Login")
-        username = st.text_input("Username")
-        passwd = st.text_input("Password",type="password")
-        # Read the Users Database
-        read_user = read_users_db("users_db",list(range(4)))
-        ippo = None
-        imps = None
-        if username:  # Check if username is not empty
-            ippo = read_user.loc[read_user['user'] == username,'ppo_cpo'].values[0]
-            imps = read_user.loc[read_user['user'] == username,'mps_cps'].values[0]
-        if st.form_submit_button("Login",type="primary"):
-            st.write(f'Logged in as {username}')
-        return ippo, imps
+# Now you can use 'hashed_passwords' as a column name
+user_dict = users.set_index('user')[['hashed_passwords', 'mps_cps']].T.to_dict()
 
-# Initialize Login
-Appo, Amps = login()
-# Appo = "COTABATO PPO"
-# Amps = "ALAMADA MPS"
-Abrgy = []
+# # Iterate over the DataFrame rows as (index, Series) pairs
+# for index, row in users.iterrows():
+#     # Use the 'user' field as the dictionary key and the other fields as values in another dictionary
+#     user_dict[row['user']] = {'password': row['passwd'], 'mps': row['mps_cps']}
 
+# Now, user_dict contains the username, password, and mps for each user
 
-
-# Get the Station Sequence
-entry_seq = read_data("station_seq", list(range(3)))
+# Convert the user_dict to the format expected by Streamlit-Authenticator
+credentials = {
+    'usernames': user_dict
+}
+print(credentials)
+# Initialize Authenticator
+authenticator = stauth.Authenticate(credentials, "kp_cases_data_entry","123", cookie_expiry_days=30)
 
 
-def new_entry_page(entry_seq, Amps):
-    st.title('Katarungang Pambarangay Cases Detailed Report Encoding')
-    st.title(f":red[{Amps} - {Appo}]")
-    # Get the Blotter Sequence
-    entry_seq = entry_seq.loc[entry_seq['mps_cps'] == Amps, 'seq'].values[0]
+authenticator.login()
+# ===========  Start FUNCTIONS  ====================
 
-    entrySeq, dateMon, monthRep ,entryNum = st.columns(4)
+# def read_users_db(worksheet, usecols):
+#     user_data = users_db.read(worksheet=worksheet, usecols=usecols)
+#     user_data = user_data.dropna(how="all")
+#     return user_data
 
-    entrySeq_value = entrySeq.text_input("Station Code", entry_seq, disabled=True, key="AentrySeq")
-
-    dateMon_value = dateMon.text_input("Year|Month",dateseq(), disabled=True)
-
-    monthRep_value = monthRep.number_input("Year|Month 'YYYYMM'",help="Enter the YEAR and MONTH the KP Incident is Reported (e.g 202105, 202212)",step=1,min_value=202101,max_value=202412)
-
-    entryNum_value = entryNum.number_input("Entry Number :red[#]",step=1,min_value=1)
-
-    combined_value = "{}-{}-{}-{}".format(entrySeq_value, dateMon_value, monthRep_value, entryNum_value)
-
-    return entryNum_value, combined_value
+# @st.cache_data(ttl=5)
+# def read_data(worksheet, usecols):
+#     data = conn.read(worksheet=worksheet, usecols=usecols)
+#     data = data.dropna(how="all")
+#     return data
 
 
-entryNum, combined_value = new_entry_page(entry_seq, Amps)
 
+# # Initialize Login
+# if not authenticator.is_authenticated():
+#     username, password = authenticator.login()
+#     # Read the Users Database
+#     read_user = read_users_db("users_db",list(range(4)))
+#     user_exists = read_user['user'] == username
+#     if user_exists.any():
+#         correct_password = read_user.loc[user_exists, 'passwd'].values[0] == password
+#         if correct_password:
+#             ippo = read_user.loc[user_exists,'ppo_cpo'].values[0]
+#             imps = read_user.loc[user_exists,'mps_cps'].values[0]
+#             st.session_state.logged_in = True
+#             st.session_state.username = username
+#             st.write(f'Logged in as {username}')
+#             st.switch_page("pages/entryCode.py")
+#         else:
+#             st.error('Incorrect password')
+#     else:
+#         st.error('User does not exist')
+# else:
+#     st.session_state.Appo
+#     st.session_state.Amps
 
-# Save combined_value in session state
-if 'combined_value' not in st.session_state:
-    st.session_state.combined_value = combined_value
-else:
-    st.session_state.combined_value = combined_value
-
-
-if st.button("New Entry",type="primary",use_container_width=True):
-    st.switch_page("pages/entryForm.py")
-
-
+# Abrgy = []
