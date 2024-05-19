6ac9c5d235dc8132a496486694742b21f4642425
Rafael Villanueva <rafael.villanueva0414@gmail.com>
2024-05-20 04:56:29 +0800

    remove footer

--- app.py
+++ app.py
@@ -13,6 +13,15 @@ css ='''
 '''
 st.markdown(css, unsafe_allow_html=True)
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 
 # Initialize Connection to the database
 conn = db_conn()
