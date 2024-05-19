807ea219f6c2c26f92bab69a41ce425d7c8b8e41
Rafael Villanueva <rafael.villanueva0414@gmail.com>
2024-05-20 05:41:19 +0800

    header

--- pages/entryForm.py
+++ pages/entryForm.py
@@ -20,6 +20,15 @@ css ='''
 '''
 st.markdown(css, unsafe_allow_html=True)
 
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 st.warning("""
            
            Please dont refresh the website to avoid resetting your encoded data \n 
