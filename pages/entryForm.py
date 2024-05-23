e9371efdce6d9be250ca390b4c60dcd13abc7d30
Rafael Villanueva <rafael.villanueva0414@gmail.com>
2024-05-23 13:14:33 +0800

    fixed time ui

--- pages/entryForm.py
+++ pages/entryForm.py
@@ -208,8 +208,8 @@ else:
 
                 time_reported_str = None
                 time_committed_str = None
-                if "input_time_reported" not in st.session_state or "input_time_committed" not in st.session_state:
-                    DTreported, DTcommitted = st.columns(2)
+                DTreported, DTcommitted = st.columns(2)
+                if "input_time_reported" not in st.session_state:
                     with DTreported:
                         st.subheader("Date & Time Reported")
                         dt_reported = st.date_input("Date Reported :red[#]",help="If di po available sa data ang exact date reported paki pili nalang po ang 1st day of the month", format="YYYY/MM/DD")
@@ -221,19 +221,7 @@ else:
                         col2.write("\n")
                         if col2.button("Enter Time",use_container_width=True, key="timereported"):
                             input_time_reported()
-
-
-                    with DTcommitted:
-                        st.subheader("Date & Time Committed")
-                        dt_committed = st.date_input("Date Committed",help="If di po available sa data ang exact date reported paki pili nalang po ang 1st day of the month",value=None, format="YYYY/MM/DD")
-                        col1 , col2 = st.columns(2)
-                        col1.text_input("Time Committed", disabled=True)
-                        col2.write("\n")
-                        col2.write("\n")
-                        if col2.button("Enter Time",use_container_width=True,key="timecommitted"):
-                            input_time_committed()
                 else:
-                    DTreported, DTcommitted = st.columns(2)
                     with DTreported:
                         st.subheader("Date & Time Reported")
                         dt_reported = st.date_input("Date Reported :red[#]",help="If di po available sa data ang exact date reported paki pili nalang po ang 1st day of the month", format="YYYY/MM/DD")
@@ -249,6 +237,18 @@ else:
                             input_time_reported()
 
 
+                if "input_time_committed" not in st.session_state:
+
+                    with DTcommitted:
+                        st.subheader("Date & Time Committed")
+                        dt_committed = st.date_input("Date Committed",help="If di po available sa data ang exact date reported paki pili nalang po ang 1st day of the month",value=None, format="YYYY/MM/DD")
+                        col1 , col2 = st.columns(2)
+                        col1.text_input("Time Committed", disabled=True)
+                        col2.write("\n")
+                        col2.write("\n")
+                        if col2.button("Enter Time",use_container_width=True,key="timecommitted"):
+                            input_time_committed()
+                else:
                     with DTcommitted:
                         st.subheader("Date & Time Committed")
                         dt_committed = st.date_input("Date Committed",help="If di po available sa data ang exact date reported paki pili nalang po ang 1st day of the month",value=None, format="YYYY/MM/DD")
